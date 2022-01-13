e7350deb73aa5ea57f443eb0f2aea34fbe8be1e6
kevingerkens <87322854+kevingerkens@users.noreply.github.com>
2022-01-13 16:14:14 +0100

    Delete Classification directory

--- Classification/clf.bat
+++
@@ -1,1 +0,0 @@
-set location=%cd% & conda activate guitarfx && python cnn_features_classification.py && python cnn_classification.py && python svm_features_classification.py && python svm_classification.py && python confusion_matrix_plots.py
